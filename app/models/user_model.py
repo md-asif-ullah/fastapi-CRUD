9994c8e7dc00e224a51825a65100f2b04a2c5b23
md-asif-ullah <mdasifullah456@gmail.com>
2025-04-03 22:57:18 +0600

    update: create and get user functionality

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel,EmailStr
 class User(BaseModel):
     name: str
     email: EmailStr
+    password:str
--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, HTTPException
 from models.user_model import User
 from db.mongo import db
 from helper.users_helper import serizlized_users_data
+from passlib.context import CryptContext
 
 
 userRoute = APIRouter(prefix="/api/v1/users",tags=["users"])
 collection= db["users"]
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 
@@ -13,9 +15,18 @@ collection= db["users"]
 async def create_user(user: User):
     try:
         user_dict = user.model_dump()
+
+        # check use exist
+        user_exist = await collection.find_one({"email": user_dict["email"]})
+        if user_exist:
+            return {"message": "user already exist"}
+
+        # encrypt password
+        user_dict["password"] = pwd_context.hash(user_dict["password"])
+
         result = await collection.insert_one(user_dict)
 
-        return {"message": "User added successfully", "data": {"id": str(result.inserted_id)}}
+        return {"message": "User added successfully", "data": {"id": [str(result.inserted_id)]}}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
@@ -23,8 +34,12 @@ async def create_user(user: User):
 @userRoute.get("/")
 async def get_user():
     all_users = await collection.find().to_list(length=None)
-    serizlized_users=[serizlized_users_data(user) for user in all_users]
-
+    serizlized_users = []
+    for user in all_users:
+        user_data = serizlized_users_data(user)
+        user_data.pop("password", None) 
+        serizlized_users.append(user_data)
+   
 
     return {"message": "Users return successfully", "data": serizlized_users}
 
